dec106d5672dbcf0d634d4d938fa36e00a26910e
mohamedalhajji <mohamedalhajji@hotmail.com>
2024-05-05 00:47:04 +0300

    updated the python version, fixed alot of bugs
    still a work in progress, not fully functional, will complete it soon

--- Networking Project/Networking Project/Networking_Project.py
+++ Networking Project/Networking Project/Networking_Project.py
@@ -9,7 +9,9 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
+#from cryptography.hazmat.primitives import padding
+#from cryptography.hazmat.primitives.padding import pad, unpad
+from cryptography.hazmat.primitives.padding import PKCS7
 from os import urandom
 
 from threading import Thread
@@ -32,18 +34,6 @@ def serialize_public_key(public_key):
         encoding=Encoding.PEM,
         format=PublicFormat.SubjectPublicKeyInfo
     )
-    
-def compute_shared_secret(private_key, peer_public_key_bytes):
-    peer_public_key = dh.DHPublicKey.from_encoded_point(parameters, peer_public_key_bytes)
-    shared_key = private_key.exchange(peer_public_key)
-    derived_key = HKDF(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=None,
-        info=b'handshake data',
-        backend=default_backend()
-    ).derive(shared_key)
-    return derived_key
 
 def get_local_ip():
     """ Get the local IP address of the computer. """
@@ -90,17 +80,19 @@ def broadcast_presence(username, subnet_mask):
                 break
 
 def listen_for_announcements():
+    local_ip = get_local_ip()
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind(('', 6000))
         while True:
             data, addr = sock.recvfrom(1024)
-            payload = json.loads(data.decode('utf-8'))
-            username = payload['username']
-            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            with peer_lock:
-                peer_dictionary[username] = (addr[0], timestamp)
-            print(f"Received announcement from {username}")
+            if addr[0] != local_ip:
+                payload = json.loads(data.decode('utf-8'))
+                username = payload['username']
+                timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                with peer_lock:
+                    peer_dictionary[username] = (addr[0], timestamp)
+                print(f"Received announcement from {username} at {addr[0]}")
 
 def tcp_server(parameters, private_key):
     host = get_local_ip()
@@ -112,7 +104,7 @@ def tcp_server(parameters, private_key):
         while True:
             conn, addr = s.accept()
             with conn:
-                peer_public_key_bytes = conn.recv(1024)  # Receive peer's public key
+                peer_public_key_bytes = conn.recv(1024)
                 derived_key = compute_shared_secret(private_key, peer_public_key_bytes, parameters)
                 while True:
                     data = conn.recv(1024)
@@ -124,42 +116,42 @@ def tcp_server(parameters, private_key):
                     except Exception as e:
                         print("Could not decrypt, showing raw message")
 
-def tcp_client(server_ip, message, secure, public_key_bytes):
+def tcp_client(server_ip, message, secure, public_key_bytes, parameters, private_key):
     port = 6001
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((server_ip, port))
-        s.sendall(public_key_bytes)  # Send public key first
+        s.sendall(public_key_bytes)
+        server_public_key_bytes = s.recv(1024)
+        derived_key = compute_shared_secret(private_key, server_public_key_bytes, parameters)
         if secure:
-            message = encrypt_message(derived_key, message)  # `derived_key` needs to be computed after receiving server's public key
+                message = encrypt_message(derived_key, message)
         s.sendall(message.encode())
         print("Message sent")
 
 def encrypt_message(key, plaintext):
-    # Generate a random IV
     iv = urandom(16)
-    # Create an AES cipher object with CBC mode
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
-    # Pad the plaintext to be a multiple of the block size
-    padded_plaintext = pad(plaintext.encode(), algorithms.AES.block_size)
-    # Encrypt the padded plaintext
+    
+    padder = PKCS7(algorithms.AES.block_size * 8).padder()
+    padded_plaintext = padder.update(plaintext.encode()) + padder.finalize()
+    
     ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
-    # Return IV + ciphertext to include IV in the transmitted message
     return iv + ciphertext
 
+
 def decrypt_message(key, iv_ciphertext):
-    # Split the IV and ciphertext
     iv = iv_ciphertext[:16]
     ciphertext = iv_ciphertext[16:]
-    # Create an AES cipher object with CBC mode
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
-    # Decrypt and unpadded the plaintext
+    
     padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-    plaintext = unpad(padded_plaintext, algorithms.AES.block_size).decode()
-    return plaintext
+    
+    unpadder = PKCS7(algorithms.AES.block_size * 8).unpadder()
+    plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
+    return plaintext.decode()
 
-from cryptography.hazmat.primitives.padding import pad, unpad
 
 def compute_shared_secret(private_key, peer_public_key_bytes, parameters):
     peer_public_key = dh.DHPublicKey.from_public_bytes(peer_public_key_bytes, parameters)
@@ -199,7 +191,6 @@ if __name__ == "__main__":
     private_key, public_key = generate_dh_keys(parameters)
     public_key_bytes = serialize_public_key(public_key)
 
-    # Threads should be adjusted to pass necessary keys
     broadcast_thread = Thread(target=broadcast_presence, args=(username, subnet_mask))
     broadcast_thread.start()
 
@@ -212,4 +203,24 @@ if __name__ == "__main__":
     while True:
         main_menu()
         command = input("Enter command: ").strip().lower()
-        # Update how you handle commands based on new setup
+        if command == 'users':
+            list_users()
+        elif command == 'chat':
+            peer = input("Enter the username of the peer to chat with: ")
+            with peer_lock:
+                if peer in peer_dictionary:
+                    peer_info = peer_dictionary[peer]
+                    last_seen = datetime.strptime(peer_info[1], '%Y-%m-%d %H:%M:%S')
+                    if (datetime.now() - last_seen).total_seconds() <= 10:
+                        message = input("Enter your message: ")
+                        secure = input("Secure chat (yes/no)? ").lower() == 'yes'
+                        tcp_client(peer_info[0], message, secure, public_key_bytes, parameters, private_key)
+                    else:
+                        print("Peer is offline or away.")
+                else:
+                    print("Peer not found.")
+        elif command == 'exit':
+            print("Exiting...")
+            break
+        else:
+            print("Unknown command. Please try again.")
